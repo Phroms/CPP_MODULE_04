2fa13b329f047f8efc5205c3a342b222beabca62
Anthony Grimaldo Ramos <hernangrim.10@gmail.com>
2024-08-27 17:52:04 +0200

    empezando ex04

---
+++ Animal.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agrimald <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/27 17:41:32 by agrimald          #+#    #+#             */
+/*   Updated: 2024/08/27 17:41:33 by agrimald         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Animal.hpp"
+
+std::string mensaje(std::string msg)
+{
+    msg = "funcionaaaa!!!!";
+
+    std::cout << "Imprimiendo mensaje: " << msg << std::endl;
+    return (msg);
+}
