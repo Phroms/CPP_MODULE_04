5c82f84e1a7a228b51310952e76b931b4a88604f
Anthony Grimaldo Ramos <hernangrim.10@gmail.com>
2024-09-04 18:49:09 +0200

    empezando ex03

---
+++ ex03/AMateria.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AMateria.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agrimald <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 18:38:20 by agrimald          #+#    #+#             */
+/*   Updated: 2024/09/04 18:38:21 by agrimald         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "AMateria.hpp"
+
+std::string mensaje(std::string msg)
+{
+    msg = "Funcionaa :D!!";
+
+    std::cout << "Imprimiendo mensaje: " << msg << std::endl;
+    return (msg);
+}
---
+++ ex03/AMateria.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AMateria.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agrimald <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 18:38:25 by agrimald          #+#    #+#             */
+/*   Updated: 2024/09/04 18:38:26 by agrimald         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef AMATERIA_HPP
+# define AMATERIA_HPP
+
+# include <iostream>
+# include <string.h>
+
+std::string mensaje(std::string msg);
+
+#endif
